f35cb06d585486b597c3d29b2bd3aff0c08a5f8f
Florian Thiel <flth@flth.de>
2025-05-17 22:52:25 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+  info = {
+    minorVersion = 0,
+    severityAdd = "NONE",
+    severityRemove = "WARNING",
+    name = _("mod_name"),
+    description = _("mod_desc"),
+    authors = {
+		{
+			name = "ModWerkstatt",
+			role = "CREATOR",
+		},
+	},
+	tags = { "plane", "flugzeug", "assets" },
+  },
+}
+end
---
+++ strings.lua
@@ -0,0 +1,21 @@
+function data()
+
+return {
+	en = {
+
+		["mod_name"] = "PZL-104 Wilga",
+		["mod_desc"] = "With this mod you get several different huts as assets for free building of a christmas market. Multiple options are included like snow, a christmas tree or a christmas crib. A nice addition for a cozy Advent Season.",
+
+        ["wilga_menu"] = "PZL-104 Wilga",
+
+	},
+    de = {
+
+	    ["mod_name"] = "PZL-104 Wilga",
+	    ["mod_desc"] = "Mit diesem Mod erhaltet ihr verschiedene Hütten als Asset für den Schönbau, einen Kiosk und diverse Accessoires für einen Weihnachtsmarkt. Optional mit Schnee oder ohne und ein Weihnachtsbaum mit Krippe ist auch mit eingepackt. Eine schöne Ergänzung für eine gemütliche Adventszeit.",
+
+        ["wilga_menu"] = "PZL-104 Wilga",
+
+	},
+}
+end
