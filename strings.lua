fda3911c6b13a0c9b84fc682df0d9acc82fc06c7
Florian Thiel <flth@flth.de>
2025-05-18 00:26:55 +0200

    Strings Platzhalter angelegt

--- strings.lua
+++ strings.lua
@@ -7,6 +7,13 @@ return {
 		["mod_desc"] = "With this mod you get several different huts as assets for free building of a christmas market. Multiple options are included like snow, a christmas tree or a christmas crib. A nice addition for a cozy Advent Season.",
 
         ["wilga_menu"] = "PZL-104 Wilga",
+        ["wilga_1_name"] = "Wilga 1",
+        ["wilga_2_name"] = "Wilga 2",
+        ["wilga_3_name"] = "Wilga 3",
+        ["wilga_4_name"] = "Wilga 4",
+        ["wilga_5_name"] = "Wilga 5",
+        ["wilga_6_name"] = "Wilga 6",
+        ["wilga_desc"] = "Wilga Desc",
 
 	},
     de = {
@@ -15,6 +22,13 @@ return {
 	    ["mod_desc"] = "Mit diesem Mod erhaltet ihr verschiedene Hütten als Asset für den Schönbau, einen Kiosk und diverse Accessoires für einen Weihnachtsmarkt. Optional mit Schnee oder ohne und ein Weihnachtsbaum mit Krippe ist auch mit eingepackt. Eine schöne Ergänzung für eine gemütliche Adventszeit.",
 
         ["wilga_menu"] = "PZL-104 Wilga",
+        ["wilga_1_name"] = "Wilga 1",
+        ["wilga_2_name"] = "Wilga 2",
+        ["wilga_3_name"] = "Wilga 3",
+        ["wilga_4_name"] = "Wilga 4",
+        ["wilga_5_name"] = "Wilga 5",
+        ["wilga_6_name"] = "Wilga 6",
+        ["wilga_desc"] = "Wilga Desc",
 
 	},
 }
